f17de7bbfa917eedc4431099bd89e112224e7cbd
Nicolas <n.ico15935746@gmail.com>
2021-05-26 03:57:32 -0500

    [Added] Load all the views for blueprint.

---
+++ views/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' Initialize blueprint object '''
+from flask import Blueprint
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/')
+from views.teams import *
+from views.users import *
+from views.repos import *
+from views.reports import *
