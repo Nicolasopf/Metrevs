6ec0dca8cb4e2af43c0e7d4adfed3d688db772e4
Nicolas <n.ico15935746@gmail.com>
2021-06-24 23:27:09 -0500

    [Added] Review view

--- views/__init__.py
+++ views/__init__.py
@@ -5,6 +5,7 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/')
 from views.teams import *
+from views.reviews import *
 from views.users import *
 from views.repos import *
 from views.reports import *
